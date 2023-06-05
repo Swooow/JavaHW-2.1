bd0a923293058e0f4ca56902f41fd6c69435a485
Swow <stepan2235@gmail.com>
2023-06-05 10:21:41 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int price = 18999;
+        int amount = 20;
+        int miles = price / amount;
+
+        System.out.println("Подравлям с покупкой билета!");
+        System.out.println("Вам начислено " + miles + " миль!");
+        System.out.println("Вы можете воспольоваться милями при оплате следующего билета.");
+        System.out.println("Подробнее с программой МИЛИ можно ознакоиться на странице ...");
+    }
+}
